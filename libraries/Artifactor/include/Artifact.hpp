3101bb3b5770a9daad22b2892f2f78860e4be919
derek-williams00 <derekOfficial00@gmail.com>
2021-06-27 16:50:27 -0700

    added some of the initial classes

---
+++ libraries/Artifactor/include/Artifact.hpp
@@ -0,0 +1,23 @@
+/*
+ *
+ */
+
+#ifndef __ARTIFACTOR_ARTIFACT_HPP
+#define __ARTIFACTOR_ARTIFACT_HPP
+
+#include "Unicore/Identifyable.hpp"
+
+namespace artifactor {
+
+
+using unicore::Identifyable;
+
+
+class Artifact {
+public:
+};
+
+
+
+}
+#endif
---
+++ libraries/Unicore/include/Identifyable.hpp
@@ -0,0 +1,26 @@
+/*
+ *
+ */
+
+#ifndef __UNICORE_IDENTIFYABLE_HPP
+#define __UNICORE_IDENTIFYABLE_HPP
+namespace unicore {
+
+
+
+
+class Identifyable {
+public:
+  virtual const string id() const = 0;
+};
+
+
+
+
+
+
+
+
+
+}
+#endif
